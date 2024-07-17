0d69e3bbc3b32c9af3139d35249fb26c347d4568
komze254 <koomedaniel045@gmail.com>
2024-07-17 21:19:55 +0300

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,28 +12,83 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+// Initialize Fancybox
+Fancybox.bind('[data-fancybox="gallery"]', {
+  // Your Fancybox configuration (if any)
+});
+
+// Initialize Swiper
 var swiper = new Swiper(".swiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  initialSlide: 2,
-  speed: 600,
-  preventClicks: true,
-  slidesPerView: "auto",
+  slidesPerView: 3, // when set to auto all the reviews are shown on the slider
+  loop: true,
+  autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+  },
   coverflowEffect: {
       rotate: 0,
       stretch: 80,
       depth: 350,
       modifier: 1,
-      slideShadows: true,
-  },
-  on: {
-      click(event) {
-          swiper.slideTo(this.clickedIndex);
-      },
+      slideShadows: false,
   },
   pagination: {
       el: ".swiper-pagination",
       clickable: true,
   },
+  on: {
+      slideChangeTransitionStart: function() {
+          this.slides.forEach(slide => {
+              if (slide.classList.contains('swiper-slide-active')) {
+                  slide.style.filter = 'none';
+                  slide.style.transform = 'scale(1)';
+                  slide.style.opacity = '1';
+              } else {
+                  slide.style.filter = 'blur(5px)';
+                  slide.style.transform = 'scale(0.8)';
+                  slide.style.opacity = '0.5';
+              }
+          });
+      },
+      init: function() {
+          this.emit('slideChangeTransitionStart');
+      }
+  }
+});
+
+// Counter Animation
+document.addEventListener("DOMContentLoaded", () => {
+  let counts = document.querySelectorAll(".num");
+
+  counts.forEach((counter) => {
+      function easeOutQuad(t) {
+          return t * (2 - t);
+      }
+
+      function animateCountUp() {
+          const target = +counter.getAttribute('data-val');
+          const duration = 2000; // duration of the animation in milliseconds
+          let start = null;
+
+          function step(timestamp) {
+              if (!start) start = timestamp;
+              const progress = timestamp - start;
+              const easeProgress = easeOutQuad(Math.min(progress / duration, 1));
+              counter.innerText = Math.floor(easeProgress * target);
+
+              if (progress < duration) {
+                  requestAnimationFrame(step);
+              } else {
+                  counter.innerText = target;
+              }
+          }
+
+          requestAnimationFrame(step);
+      }
+
+      animateCountUp();
+  });
 });
